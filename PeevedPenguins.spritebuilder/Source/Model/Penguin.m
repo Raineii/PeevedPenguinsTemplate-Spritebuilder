79a1edd14a6cf186ee163ad5ab9dec30652b0217
Raineii <pablohrdz@gmail.com>
2014-07-11 01:41:19 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Model/Penguin.m
@@ -0,0 +1,24 @@
+//
+//  Penguin.m
+//  PeevedPenguins
+//
+//  Created by Pollo on 11/07/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Penguin.h"
+
+@implementation Penguin
+
+-(instancetype)init{
+    
+    self = [super init];
+    
+    if(self){
+        CCLOG(@"Penguin created");
+    }
+    
+    return self;
+}
+
+@end
---
+++ PeevedPenguins.spritebuilder/Source/Model/Seal.m
@@ -0,0 +1,24 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by Pollo on 11/07/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+
+-(instancetype)init{
+    
+    self = [super init];
+    
+    if(self){
+        CCLOG(@"Seal created");
+    }
+    
+    return self;
+}
+
+@end
