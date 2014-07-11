e7d76598a6d33d8d035b8e743c4f16a8da072005
Raineii <pablohrdz@gmail.com>
2014-07-11 05:11:48 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Model/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Model/Gameplay.m
@@ -13,6 +13,7 @@
     CCNode *_catapultArm;
     CCNode *_levelNode;
     CCNode *_contentNode;
+    CCNode *_pullbackNode;
     
 }
 
@@ -26,6 +27,7 @@
     
     //Visualize physics bodies and joints
     _physicsNode.debugDraw = TRUE;
+    _pullbackNode.physicsBody.collisionMask = @[];
 }
 
 //Called on every touch in this scene
